3c0c989939aeb48c2053a84afac836ed8a9aa6b1
Brad <kingdomlandscapevt@gmail.com>
2021-10-23 11:27:38 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,122 @@
+// Assignment Code
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+
+
+var generateBtn = document.querySelector("#generate");
+function generatePassword(){
+  // generatePassword 8-128 characters
+  //  what type of character does the user want
+  var passwordLength= prompt("what length does user want between 8-128 characters")
+  var confirmUpperCase= confirm( "does user want to use uppercase")
+  var confirmLowerCase= confirm( "does user want to use lower case")
+  var confirmSpecialCharacters= confirm( "does user want special characters")
+  var confirmNumericCharacters= confirm( "does user want numeric characters")
+
+var useroptions= {
+  passwordLength:passwordLength,
+  confirmLowerCase:
+  confirmUpperCase:
+  confirmNumericCharacters:
+  confirmSpecialCharacters:
+
+
+}
+
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
